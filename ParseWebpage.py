a78cc6a0d022e5f91d4d15712faf60475e0a10a7
Chris541500 <chriscapaldi1990@gmail.com>
2024-09-30 21:08:50 -0400

    api_data: simplify get function

--- ParseWebpage.py
+++ ParseWebpage.py
@@ -3,27 +3,18 @@ from urllib.request import Request, urlopen
 import json
 
 ADDRESS = "https://pokeapi.co/api/v2/{}/{}/"
-POKEMON = "pokemon"
-SPECIES = "pokemon-species"
 HEADERS = {'User-Agent': 'Mozilla/5.0'}
 
 
-def get_pkmn_api_data(entry_value):
-    print(f"Loading Data for {entry_value}...")
-    pkmn_address = ADDRESS.format(POKEMON, entry_value)
-    species_address = ADDRESS.format(SPECIES, entry_value)
-
-    pkmn_request = Request(url=pkmn_address, headers=HEADERS)
-    species_request = Request(url=species_address, headers=HEADERS)
+def get_api_data(entry_value, resource):
+    address = ADDRESS.format(resource, entry_value)
+    request = Request(url=address, headers=HEADERS)
 
     try:
-        pkmn_raw_data = urlopen(pkmn_request).read()
-        species_raw_data = urlopen(species_request).read()
+        raw_data = urlopen(request).read()
+        data = json.loads(raw_data)
 
-        pkmn_data = json.loads(pkmn_raw_data)
-        species_data = json.loads(species_raw_data)
-
-        return pkmn_data, species_data
+        return data
 
     except HTTPError as http:
         print(http)
