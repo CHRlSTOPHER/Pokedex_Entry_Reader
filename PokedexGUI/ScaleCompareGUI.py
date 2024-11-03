f7264d1adef68aa9704ca028f537d16ad29a7b7e
Christopher <chriscapaldi1990@gmail.com>
2024-11-03 13:06:45 -0500

    gui: adjust scaling process

--- PokedexGUI/ScaleCompareGUI.py
+++ PokedexGUI/ScaleCompareGUI.py
@@ -8,10 +8,6 @@ from PokedexGUI.GlobalGUI import BG_COLOR
 TRAINER_IMG_PATH = "resources/icons/trainer-icon.png"
 
 SCALE = 200
-POKEMON_HEIGHT = SCALE
-HUMAN_HEIGHT = SCALE
-POKEMON_WIDTH = SCALE
-HUMAN_WIDTH = SCALE
 
 AVERAGE_HUMAN_SIZE = 1.7
 
@@ -29,6 +25,7 @@ class ScaleCompareGUI(tb.LabelFrame):
         self.grid(column=0, row=0)
         self.grid_propagate(False)  # freeze the frame size
         self.artwork = None
+        self.pokemon_height = 0
 
         self.generate()
 
@@ -40,49 +37,23 @@ class ScaleCompareGUI(tb.LabelFrame):
         self.human_canvas.configure(background=BG_COLOR)
 
     def load_scale_compare(self, pokemon_height, artwork):
-        self.artwork = artwork
-        pokemon_height = pokemon_height
-        pokemon_scale = 1.0
-        human_scale = 1.0
+        self.pokemon_height = pokemon_height
+        self.reset_canvas()
+        human_image = Image.open(TRAINER_IMG_PATH)
+        pokemon_image = self.load_pokemon_image(artwork)
+        human_image, pokemon_image = self.resize_images(human_image,
+                                                        pokemon_image)
+        self.render_images(human_image, pokemon_image)
 
-        if pokemon_height > AVERAGE_HUMAN_SIZE:
-            human_scale = AVERAGE_HUMAN_SIZE / pokemon_height
-        elif AVERAGE_HUMAN_SIZE > pokemon_height:
-            pokemon_scale = pokemon_height / AVERAGE_HUMAN_SIZE
-
-        p_height = POKEMON_HEIGHT * pokemon_scale
-        p_width = POKEMON_WIDTH * pokemon_scale
-        h_height = HUMAN_HEIGHT * human_scale
-        h_width = HUMAN_WIDTH * human_scale
-
-        # reset canvas
+    def reset_canvas(self):
         self.pokemon_canvas.grid_forget()
         self.human_canvas.grid_forget()
         self.pokemon_canvas.grid(row=0, column=0, sticky='se', padx=(10, 10))
         self.human_canvas.grid(row=0, column=1, sticky='ws', padx=(10, 10))
 
-        x_scale, y_scale = self.load_pokemon_photo(p_width, p_height)
-        self.load_human_photo(h_width, h_height, x_scale, y_scale)
-
-    def load_human_photo(self, width, height, x_scale, y_scale):
-        # match the scale of the trainer with the pokemon's scale modifiers
-        if x_scale != 1.0:
-            width *= x_scale
-            height *= x_scale
-        elif y_scale != 1.0:
-            width *= y_scale
-            height *= y_scale
-
-        # load canvas images
-        image = Image.open(TRAINER_IMG_PATH)
-        image = image.resize((int(width/2), int(height)))
-        photo = ImageTk.PhotoImage(image)
-
-        self.human_canvas.configure(image=photo)
-        self.human_canvas.image = photo
-
-    def load_pokemon_photo(self, width, height):
-        reg_art, shiny_art = self.artwork
+    def load_pokemon_image(self, artwork):
+        # Open the image
+        reg_art, shiny_art = artwork
         raw_data = urllib.request.urlopen(reg_art).read()
         image = Image.open(io.BytesIO(raw_data))
 
@@ -91,29 +62,61 @@ class ScaleCompareGUI(tb.LabelFrame):
         alpha_thresh = alpha.point(lambda p: 255 if p > 200 else 0)
 
         # Make a new completely black image same size as original
-        black_copy = Image.new('RGB', image.size)
+        silhouette = Image.new('RGB', image.size)
 
         # Copy across the alpha channel from original
-        black_copy.putalpha(alpha_thresh)
+        silhouette.putalpha(alpha_thresh)
 
+        # remove any excess empty space from the sides of the image
         bounding_box = image.getbbox()
-        image = black_copy.crop(bounding_box)
+        pokemon_image = silhouette.crop(bounding_box)
 
-        # preserve resolution ratio
-        x_scale = 1.0
-        y_scale = 1.0
-        x, y = image.size
-        if y > x:
-            x_scale = x / y
-        elif x > y:
-            y_scale = y / x
-        width *= x_scale
-        height *= y_scale
+        return pokemon_image
 
-        image = image.resize((int(width), int(height)))
-        photo = ImageTk.PhotoImage(image)
+    def resize_images(self, human_image, pokemon_image):
+        human_image_width, human_image_height = human_image.size
+        pokemon_image_width, pokemon_image_height = pokemon_image.size
 
-        self.pokemon_canvas.configure(image=photo)
-        self.pokemon_canvas.image = photo
+        # First, we make sure the human and pokemon are matching in height.
+        divider = human_image_height / pokemon_image_height
+        human_image_height /= divider
+        human_image_width /= divider
 
-        return x_scale, y_scale
+        # Second, we get the scale ratio between them based on their height
+        # the shorter entity will be scaled down.
+        human_scale = 1.0
+        pokemon_scale = 1.0
+        if self.pokemon_height > AVERAGE_HUMAN_SIZE:
+            human_scale = AVERAGE_HUMAN_SIZE / self.pokemon_height
+        elif AVERAGE_HUMAN_SIZE > self.pokemon_height:
+            pokemon_scale = self.pokemon_height / AVERAGE_HUMAN_SIZE
+
+        # Third, scale them both down to fit in the labelframe
+        # preserve the scale difference during this process
+        # get the biggest length out of the two images
+        max_length = max(human_image_width, human_image_height,
+                         pokemon_image_width, pokemon_image_height)
+        scale_down = SCALE / max_length
+
+        # Third, apply the scales to the image resolutions
+        human_image_width *= (human_scale * scale_down)
+        human_image_height *= (human_scale * scale_down)
+        pokemon_image_width *= (pokemon_scale * scale_down)
+        pokemon_image_height *= (pokemon_scale * scale_down)
+
+        # Lastly, apply the new scale values to the images themselves
+        human_image = human_image.resize((int(human_image_width),
+                                          int(human_image_height)))
+        pokemon_image = pokemon_image.resize((int(pokemon_image_width),
+                                              int(pokemon_image_height)))
+
+        return human_image, pokemon_image
+
+    def render_images(self, human_image, pokemon_image):
+        human_photo = ImageTk.PhotoImage(human_image)
+        self.human_canvas.configure(image=human_photo)
+        self.human_canvas.image = human_photo
+
+        pokemon_photo = ImageTk.PhotoImage(pokemon_image)
+        self.pokemon_canvas.configure(image=pokemon_photo)
+        self.pokemon_canvas.image = pokemon_photo
