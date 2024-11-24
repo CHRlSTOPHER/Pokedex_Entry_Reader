32dc0305f84c7820e45b1c008e6c4b7e7b5b369f
Christopher <chriscapaldi1990@gmail.com>
2024-11-24 16:30:49 -0500

    search_gui: start dropdown menus

--- PokedexGUI/SearchGUI.py
+++ PokedexGUI/SearchGUI.py
@@ -1,3 +1,5 @@
+from tkinter import StringVar
+
 import ttkbootstrap as tb
 from ttkbootstrap import DISABLED
 
@@ -11,6 +13,8 @@ FONT_SIZE = 11
 # button area size
 AREA = 20
 ENABLED = "normal"
+ALPHABET = "abcdefghjijklmnopqrstuvwxyz"
+DEFAULT_LETTER = 'B'
 
 class SearchGUI(tb.Frame):
 
@@ -22,42 +26,54 @@ class SearchGUI(tb.Frame):
         self.search_entry = None
         self.searches = []
         self.search_index = -1
-        self.alphabet = "abcdefghjijklmnopqrstuvwxyz"
+        self.alphabet_list = []
         self.alphabet_dict = {}
-        self.letter = 'b'
+        self.letter = DEFAULT_LETTER
 
         self.generate()
 
     def generate(self):
-        # equal column weight
+        # equal column and row weight
         [self.columnconfigure(c, weight=1) for c in range(7)]
         self.rowconfigure(0, weight=1)
 
+        self.generate_back_forward_gui()
+        self.generate_search_gui()
+        self.generate_drop_down_gui()
+
+    def generate_back_forward_gui(self):
         # Move between Pokemon searches (left arrow - previous searches)
         # (right arrow - recent searches. right works after pressing left)
         self.left_button = tb.Button(self, text=' <', style='button.TButton')
         self.right_button = tb.Button(self, text=' >', style='button.TButton')
         self.left_button.grid(column=0, row=0, padx=(10, 0), sticky='w')
-        self.right_button.grid(column=1, row=0, padx=(0, 5), sticky='w')
+        self.right_button.grid(column=1, row=0, padx=(0, 0), sticky='w')
         self.left_button["state"] = DISABLED
         self.right_button["state"] = DISABLED
 
-        # Search Label and Entry
+    def generate_search_gui(self):
         self.search_label = tb.Label(self, text="Search:",
                                      background=BG_COLOR, foreground=FG_COLOR,
                                      font=(FONT, FONT_SIZE, "bold"))
-        self.search_label.grid(column=2, row=0, sticky='e', padx=(0, 3))
+        self.search_label.grid(row=0, column=2, sticky='e', padx=(0, 3))
 
         self.search_entry = tb.Entry(self, width=12)
-        self.search_entry.grid(sticky='w', row=0, column=3)
+        self.search_entry.grid(row=0, column=3, sticky='w')
 
-        # Alphabetical Dropdown menus
+    def generate_drop_down_gui(self):
+        self.create_alphabet_collections()
 
+        # drop down gui
+        letter = StringVar()
+        letter.set(self.letter)
+        self.alphabet_menu = tb.OptionMenu(self, letter, self.letter,
+                                           *self.alphabet_list)
+        self.alphabet_menu.grid(row=0, column=4)
 
-        self.create_alphabet_dict()
-
-    def create_alphabet_dict(self):
-        for letter in self.alphabet:
+    def create_alphabet_collections(self):
+        for letter in ALPHABET:
+            letter = letter.upper()
+            self.alphabet_list.append(letter)
             self.alphabet_dict[letter] = []
 
     def append_to_search_list(self, query):
